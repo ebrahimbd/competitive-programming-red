817339469c974e1ed96945ceb2df5542d84fa423
ebrahimbd <ebrahim16sust@gmail.com>
2022-02-10 23:19:04 +0600

    diameter of tree

--- tree.py
+++ tree.py
@@ -2,46 +2,75 @@ class Head:
 
     def __init__(self, key):
         self.key=key
-        self.leftnode=None
-        self.rightnode=None
+        self.left=None
+        self.right=None
 
     def insert(self, data):
         if self.key is None:
             self.key=data
-            print("if self.key is None:")
             return
         if self.key==data:
-            print("self.key==data")
             return
         if self.key > data:
-            if self.leftnode:
-                print("self.leftnode")
-                self.leftnode.insert(data)
+            if self.left:
+                self.left.insert(data)
             else:
-                print("self.leftnode=Head(data)")
-                self.leftnode=Head(data)
+                self.left=Head(data)
         else:
-            if self.rightnode:
-                print("self.rightnode:")
-                self.rightnode.insert(data)
+            if self.right:
+                self.right.insert(data)
             else:
-                print("self.rightnode=Head(data)")
-                self.rightnode=Head(data)
-       
+                self.right=Head(data)
+                
+    def diameterOfBinaryTree(self, root):
+        tree=root
+        longestDiameter = [0]
+        longestDiameter[0]=self.binaryTreeHelper(tree.left, longestDiameter )
+        return longestDiameter[0]
+    	
+    def binaryTreeHelper(self, tree, longestDiameter):
+            dia=0
+            if tree.left is not None:
+                while tree.left:
+                    dia+=1
+                    if tree.right is None:
+                        return binaryTreeHelper(self, tree, longestDiameter)  
+                        
+            elif tree.right is not None:
+                while tree.right:
+                    dia+=1
+                    if tree.right is None:
+                       return  binaryTreeHelper(self, tree, longestDiameter)  
+                        
+         
+            return dia
+            
+            
+        # if tree.right is None:
+        #     return 0
+        # maxLeftPath =self.binaryTreeHelper(tree.left, longestDiameter)
+        # if maxLeftPath is None:
+        #     maxLeftPath=0
+        #     return maxLeftPath
+        # maxRightPath =self.binaryTreeHelper(tree.right, longestDiameter)
+        # if maxRightPath is None:
+        #     maxRightPath=0
+        #     return maxRightPath
+        # maxCurrentPathLength = maxLeftPath + maxRightPath
+        # longestDiameter[0] = max(longestDiameter[0], maxCurrentPathLength)
+        # return max(1 + maxLeftPath, 1 + maxRightPath)
+      
             
         
-
 root=Head(99)
-list=[6,2,10,9,98]
-for i in list:
+list1=[1,2,3,4,5]
+for i in list1:
     root.insert(i)
-    
-    
-    
-    
-root=Head(99)
-list=[6,2,10,9,98]
-for i in list:
-    root.insert(i)                            
 
+print(root.diameterOfBinaryTree(root))
+    
+ 
+
+ 
+ 
 
