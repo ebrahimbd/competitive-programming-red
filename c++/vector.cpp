bd6c6fc05d60360cb4498fde35376cc0aaca614d
ebrahimbd <ebrahim16sust@gmail.com>
2022-02-23 19:33:40 +0600

    learn vector

---
+++ c++/vector.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+  
+using namespace std;
+  
+int main()
+{
+    vector<int>eb;
+string a="lfdjkldfjdlf";
+
+string *b=&a;
+
+string *bb=b;
+
+
+
+cout<<*bb;
+
+
+    for(int i=0;i<100; i++){
+        eb.push_back(i);
+    }
+    for (int i=0; i<5; i++){
+        cout<<eb.at(i)<<endl;
+    }
+    cout<<eb.at(3)<<endl;
+}
