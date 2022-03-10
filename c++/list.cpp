c8036eb951fb8ca9cf9dfbe288435828489ecf19
ebrahimbd <ebrahim16sust@gmail.com>
2022-03-10 21:30:45 +0600

    implement  a list c++

---
+++ c++/list.cpp
@@ -0,0 +1,16 @@
+// CPP program to show the implementation of List
+#include <iostream>
+#include <iterator>
+#include <list>
+using namespace std;
+ 
+// function for printing the elements in a list
+list<int>e; 
+// Driver Code
+int main()
+{
+ 
+   e.push_back(555555);
+   cout<<e.back();
+   return 0;
+}
