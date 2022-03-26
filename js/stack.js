8575c822ba63a7ee5b1759d86d0df27a9f40a69f
ebrahimbd <ebrahim16sust@gmail.com>
2022-03-26 13:23:59 +0600

    stack implimention in python and javascript

---
+++ js/stack.js
@@ -0,0 +1,7 @@
+var i=['rrr','rrr'];
+i.push('sfs');
+
+var g=['dfd'];
+
+i.push(g);
+console.log(i.pop(0));
---
+++ stack.py
@@ -0,0 +1,16 @@
+# stack using list
+stack = ["Amar", "Akbar", "Anthony"]
+stack.append("Ebrahim")
+print(stack)
+
+# Removes the last item
+print(stack.pop())
+
+print(stack)
+
+# Removes the last item
+print(stack.pop())
+
+print(stack)
+
+
