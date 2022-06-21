792fdcfe76d8780270877228aa5d5f91f77be830
ebrahimbd <ebrahim16sust@gmail.com>
2022-06-21 18:45:50 +0600

    binary search in order

--- c++/new.cpp
+++ c++/new.cpp
@@ -1,4 +1,7 @@
 #include<iostream>
+#include <vector>
+
+std::vector <int> v;	
 
 struct Node {
     int data;
@@ -7,35 +10,57 @@ struct Node {
 };
 
 Node * Newnode(int data){
-    Node *root=new Node();
-    root->data=data;
-    root->left=root->right=NULL;
+    Node *root = new Node();
+    root->data = data;
+    root->left = root->right=NULL;
     return root;
 }
 
 Node * insert(Node *root, int data){
 	   
     if(root==NULL){
-		std::cout<<data;
-        std::cout<<"-main \n";
+		//std::cout<<data;
+        //std::cout<<"-main \n";
        root= Newnode(data);
-    }else if(data > root->data){
-		std::cout<<data;
-        std::cout<<"-right \n";
-        root=root->right=Newnode(data);
+    }else if(data==root->data){
+		return root;
+		}else if(data >root->data){
+		//std::cout<<data;
+        //std::cout<<"-right \n";
+        root->right=insert(root->right, data);
     }else{
-		std::cout<<data;
-        std::cout<<"-left\n";
-      root=root->left=Newnode(data);
+		//std::cout<<data;
+        //std::cout<<"-left\n";
+        root->left=insert(root->left, data);
     }
     return root;
-}
+};
 
 
-Node *postorder(Node * root, int data){
-	
-		return 0;
-	
+bool search(Node * root, int data){
+	   if(root==NULL){
+		   std::cout<<"There is no node \n";
+		    return false;
+	   }
+	   else if(root->data == data){
+		   //std::cout<<data;
+		   return true;
+	   }else if(data >root->data){
+		  return search(root->right, data);
+	   }else{
+		  return search(root->left, data);
+	   }
+	};
+ 
+
+void postorder(Node * root){
+	  if (root==NULL){
+		  return ;
+	  }
+	  postorder(root->left);
+	  v.push_back(root->data);
+	  postorder(root->right);
+	  
 	};
 
 
@@ -44,4 +69,19 @@ int main(){
     ami=insert(ami, 5);
     ami=insert(ami, 400);
     ami=insert(ami, 100);
+    ami=insert(ami, 100);
+    ami=insert(ami, 600);
+
+    postorder(ami);
+    
+    for(int i: v){
+		std::cout<<i<<"\n";
+	}
+   
+    if(search(ami, 400) == true) {
+		std::cout<<"Found\n";
+		}
+	else{
+		 std::cout<<"Not Found\n";
+		 }
 }
