836383dacb9f77e98f1bb9e72261066a646d1a70
ebrahimbd <ebrahim16sust@gmail.com>
2022-07-25 02:56:05 +0600

    insert a linked list in n position

---
+++ c++/practice/delete_nth_position_linkedlist.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+#define ll   long long
+
+#define fastread()  (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+using namespace std;
+
+
+struct Node{
+    int data;
+    Node *next;
+};
+
+struct Node *head;
+
+void insert(int data){
+   Node *newnode=new Node();
+   newnode->data=data;
+   newnode->next=head;
+   head=newnode;
+};
+
+void print(){
+     Node *node=head;
+    while(node!=NULL){
+        cout<<node->data<<"->";
+        node=node->next;
+    }
+    cout<<endl;
+};
+
+void delete_node(int n){
+    struct Node * temp1=head;
+    if(n==1){
+        head=temp1->next;
+        free(temp1);
+        return;
+    }
+    for (int i=0; i<n-2; i++){
+        temp1=temp1->next;
+    }
+    struct Node *temp2=temp1->next;
+    temp1->next=temp2->next;
+    free(temp2);
+};
+
+
+void node_insert_n_position(int n, int data){
+    Node *temp=head;
+    if(n==1){
+        temp->data=data;
+        temp->next=head;
+        head=temp;
+        return;
+    }
+
+    for (int i=0; i<n-2; i++){
+        temp=temp->next;
+    }
+    Node *newnode=new Node();
+    newnode->data=data;
+    newnode->next=temp->next;
+    temp->next=newnode;
+}
+
+
+int main(){
+     head=NULL;
+    for(int i=0; i<10; i++){
+        insert(i);
+    }
+    print();
+   delete_node(4);
+   print();
+   node_insert_n_position(4, 444);
+   print();
+   return 0;
+}
