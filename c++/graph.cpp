5a83203461ab76623bad2d65165fd22c8e7e6635
ebrahimbd <ebrahim16sust@gmail.com>
2022-07-31 00:48:27 +0600

    graph DFS implement

--- c++/graph.cpp
+++ c++/graph.cpp
@@ -1,26 +1,54 @@
 #include<bits/stdc++.h>
+#define ll   long long
 
+#define fastread()  (ios_base:: sync_with_stdio(false),cin.tie(NULL));
 using namespace std;
 
-vector <int> v[10000];
+const int N=1e5+10;
+vector <int>g[N];
+
+void insert_graph(vector <int> g[], int edge){
+    for(int i=0; i<edge; i++){
+        int x, y;
+        cin>>x>>y;
+        g[x].push_back(y);
+        g[y].push_back(x);
+    }
+}
+
+void print_graph(vector <int> g[], int vertex){
+    cout<<endl<<endl;
+    for(int i=1; i<=vertex; i++){
+        cout<<"["<<i<<"]";
+        for(int j=0; j<size(g[i]); j++){
+            cout<<"->["<<g[i][j]<<"]";
+        }cout<<endl;
+    }
+}
+
+bool vis[N];
+
+void dfs(int vertex){
+    if (vis[vertex]){
+        cout<<"visit node "<<vertex <<" is true so return "<<"parent node"<<endl;
+        return;
+    }
+    vis[vertex]=true;
+    for(int child : g[vertex]){
+    cout<<"visit node-"<<vertex<<"-child->"<<child<<"<-node  so call  "<<"dfs("<<child<<")"<<endl;
+        dfs(child);
+    }
+}
 
 int main(){
-    int n, m;
-    cin>>n>>m;
-    int x, y;
-    while(m--){
-        cin>>x>>y;
+    fastread();
+    int vertex, edge;
+    cin>>vertex>>edge;
+    insert_graph(g, edge);
+    print_graph(g, vertex);
+    cout<<endl;
+    dfs(1); 
+    cout<<endl; 
 
-        v[x].push_back(y);
-        v[y].push_back(x);
-    }
-
-    for(int i=0; i<n; i++){
-        cout<<i;
-        for(int j=0; j<v[i].size(); j++){
-        cout<<"->"<<v[i][j];
-        }
-        cout<<endl;
-    }
-return 0;
+    return 0;
 }
