6278251ec7cabefa47db32d8aa33de729eb67b12
ebrahimbd <ebrahim16sust@gmail.com>
2022-08-28 02:08:34 +0600

    knapcak in dp table

--- Codeforce/123. The sum.py
+++ Codeforce/123. The sum.py
@@ -5,18 +5,24 @@ Created on Thu Jun 16 03:31:26 2022
 
 @author: ebrahim
 """
-n=int(input())
-#This code gives nth number in fibonacci series - {0,1,1,2,3,5,......}.
-def fibonacci(n, f1, f2, after):
-    sum=0
-    while f1<=n:
-        sum=f1+sum
-        after= f1+f2
-        f1=f2
-        f2=after
-    return sum
- 
-if __name__ == "__main__":
-    print(fibonacci(n, 0, 1,after=0))
-  
-# This code is contributed by Manan Tyagi.
+items = [1,1,3,4,5]
+knapsack = []
+limit = 7
+
+def print_solutions(current_item, knapsack, current_sum):
+    #if all items have been processed print the solution and return:
+    if current_item == len(items):
+        print (knapsack)
+        return
+
+    #don't take the current item and go check others
+    print_solutions(current_item + 1, list(knapsack), current_sum)
+
+    #take the current item if the value doesn't exceed the limit
+    if (current_sum + items[current_item] <= limit):
+        knapsack.append(items[current_item])
+        current_sum += items[current_item]
+        #current item taken go check others
+        print_solutions(current_item + 1, knapsack, current_sum )
+
+print_solutions(0,knapsack,0)
---
+++ c++/DP/table_napsack.cpp
@@ -0,0 +1,80 @@
+#include<bits/stdc++.h>
+#define ll   long long
+
+#define fastread()  (ios_base:: sync_with_stdio(false),cin.tie(NULL));
+using namespace std;
+
+int main(){
+    fastread();
+    int N, W;
+    cin>>N>>W;
+    vector<ll>wi(N);
+    vector<ll>val(N);
+    for(int i=0; i<N; i++){
+        cin>>wi[i]>>val[i];
+    }
+
+    vector<vector<ll>>k(N+1, vector<ll>(W+1)); 
+
+    for(int i=0; i<=N; i++){
+        for(int j=0; j<=W; j++){
+            if(i==0||j==0){
+                k[i][j]=0;
+            }else if(wi[i-1]<=j){
+                k[i][j]=max(val[i-1]+k[i-1][j-wi[i-1]], k[i-1][j]);
+            }else{
+                k[i][j]=k[i-1][j];
+            }
+        }
+    }
+    cout<<k[N][W]<<endl;
+
+    cout<<endl;
+   for(int i = 0; i <= N; i++)
+    {
+        for(int j = 0; j <= W; j++)
+        {
+           cout << k[i][j] << " ";
+        }
+        cout << endl;
+    }
+     
+
+   cout<<endl;
+/*
+    int res = k[N][W];
+    cout<< res << endl;
+     
+    int  w = W;
+    for (int i = N; i > 0 && res > 0; i--) {
+        if (res == k[i - 1][w])
+            continue;   
+        else {
+            cout<<" "<<wi[i - 1] ;
+            res = res - val[i - 1];
+            w = w - wi[i - 1];
+        }
+    }
+
+*/
+
+   vector <int >ans;
+   pair<int, int> list[N];
+int v=k[N][W];
+   for(int i=N; i>0; i--){
+       for(int j=W; j>0; j--){
+           if(k[i-1][j]==v){
+               ans.push_back(wi[i]);
+               v=k[i-1][j-1];
+           }else{
+               v=k[i][j-1];
+           }
+       }
+   }
+  
+   for(auto i: ans){
+       cout<<i<<endl;
+   }
+
+    return 0;
+}
